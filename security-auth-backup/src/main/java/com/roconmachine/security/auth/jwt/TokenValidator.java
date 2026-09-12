96cf0a52bade78bc8c7b07c7b719b7d1ef299d41
ROCON <roconmachine@gmail.com>
2026-09-12 23:35:07 +0600

    backup security-auth-back deleted

--- security-auth-backup/src/main/java/com/roconmachine/security/auth/jwt/TokenValidator.java
+++
@@ -1,8 +0,0 @@
-package com.roconmachine.security.auth.jwt;
-
-import com.roconmachine.security.auth.model.AuthenticatedPrincipal;
-
-public interface TokenValidator {
-
-   AuthenticatedPrincipal validate(String token) throws TokenValidationException;
-}
